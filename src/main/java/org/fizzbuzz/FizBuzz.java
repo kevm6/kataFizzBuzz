fff34fb39ac67bd7783e6b6ed9fc93b3531fe091
Kev <niggiz97@gmail.com>
2018-03-28 21:49:27 +0200

    Gerer les nombres non multiple de 3 et 5

--- src/main/java/org/fizzbuzz/FizBuzz.java
+++ src/main/java/org/fizzbuzz/FizBuzz.java
@@ -2,4 +2,8 @@ package org.fizzbuzz;
 
 public class FizBuzz {
 
+	public static String execute(String number) {
+		return number;
+	}
+
 }
--- src/test/java/org/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,23 @@
 package org.fizzbuzz;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class FizzBuzzTest {
 
+	@Test
+	public void shouldReturnOne() {
+		assertEquals("1",FizBuzz.execute("1"));
+	}
+	
+	@Test
+	public void shouldReturnTwo() {
+		assertEquals("2",FizBuzz.execute("2"));
+	}
+	
+	@Test
+	public void shouldReturnFizz() {
+		assertEquals("Fizz",FizBuzz.execute("3"));
+	}
 }
